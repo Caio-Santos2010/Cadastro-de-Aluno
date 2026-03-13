a02b3e14b297844163b5f3cdc05ac920d808453d
Caio-Santos2010 <caio.santos.amaral@escola.pr.gov.br>
2026-03-13 10:53:20 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,4 +11,24 @@ const conexao = mysql.createConnection({
     user:'root',
     password: '',
     database: 'escola'
+
+});
+
+app.post("/salvar", (req, res) => {
+    let nome = req.body.nome;
+    let idade = req.body.idade;
+
+    let sql = "INSERT INTO alunos (nome, idade) VALUES (?, ?);"
+
+    conexao.query(sql, [nome, idade], (erro, resultado) => {
+        if(erro) {
+            console.log(erro);
+        } else {
+            res.send("Alunos salvo com sucesso");
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
 });
